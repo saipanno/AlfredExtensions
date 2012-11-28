a4b908b8a8f1999cda3f864ed8c7fe02ff8c8e7e
saipanno <kid.xiyang@gmail.com>
2012-11-28 22:58:28 +0800

    增加AirPort设备支持

--- QuickWIFI/QuickWIFI.sh
+++ QuickWIFI/QuickWIFI.sh
@@ -1,11 +1,11 @@
-DEVICE=$(networksetup -listallhardwareports | grep -A 2 -E "AirPort|Wi-Fi" | grep -m 1 -o -e en[0-9]);
+DEVICE=$(networksetup -listnetworkserviceorder | awk '/Hardware Port: Wi-Fi/ { print $5 }');
 case "{query}" in
     "on")
-        networksetup -setairportpower $DEVICE On
+        networksetup -setairportpower ${DEVICE%)*} On
         echo "Wi-Fi enabled."
     ;;
     "off")
-        networksetup -setairportpower $DEVICE Off
+        networksetup -setairportpower ${DEVICE%)*} Off
         echo "Wi-Fi disabled."
     ;;
 esac
